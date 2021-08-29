6b0979e34637ff807dfe845aa1ac4a0b66932d80
ideavanicha <ideavanicha@gmail.com>
2021-08-29 19:49:27 +1000

    sticky

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -19,7 +19,6 @@ export default function NavigationBar() {
 
         if (collasped){
             document.querySelector(".drop-down").style.display = "flex";
-            console.log(document.querySelector(".drop-down").style.display)
             setCollasped(false)
         } else {
             document.querySelector(".drop-down").style.display = "none";
@@ -27,46 +26,31 @@ export default function NavigationBar() {
         }
     }
 
-    // const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    // useEffect(() => {
-    //   function handleResize() {
-    //     setWindowDimensions(getWindowDimensions());
-
-    //     if (windowDimensions.width == 1500){
-    //         document.querySelector(".navbar-container").style.display = "flex";
-    //         setCollasped(false)
-    //     }
-    //   }
-  
-    //   window.addEventListener('resize', handleResize);
-    //   return () => window.removeEventListener('resize', handleResize);
-    // }, []);
-
     return(
-        <div>
+        <div class="container">
             <div class="navbar-container">
                 <Link to="home-container" smooth={true} class="navbar-logo">Idea</Link>
-            <div class="navbar-links">
-                <Link to="about-container" smooth={true} class="links">About Me</Link>
-                <Link to="overall-container"  smooth={true} class="links">Education</Link>
-                <Link to="overall-container" smooth={true} class="links">Experience</Link>
-                <Link to="projects-container" smooth={true} class="links">Projects</Link>
-                <Link to="footer-container" smooth={true} class="links">Contact Me</Link>
+                <div class="navbar-links">
+                    <Link to="about-container" smooth={true} class="links">About Me</Link>
+                    <Link to="overall-container"  smooth={true} class="links">Education</Link>
+                    <Link to="overall-container" smooth={true} class="links">Experience</Link>
+                    <Link to="projects-container" smooth={true} class="links">Projects</Link>
+                    <Link to="footer-container" smooth={true} class="links">Contact Me</Link>
+                </div>
             </div>
-        </div>
         <div class="drop-down">
                 <Link to="about-container" smooth={true} class="drop">About Me</Link>
                 <Link to="overall-container"  smooth={true} class="drop">Education</Link>
                 <Link to="overall-container" smooth={true} class="drop">Experience</Link>
                 <Link to="projects-container" smooth={true} class="drop">Projects</Link>
                 <Link to="footer-container" smooth={true} class="drop">Contact Me</Link>
-            </div>
-        <div class="menu">{ collasped ?
-        <AiOutlineMenu size={30} color="black" onClick={showNav}/> :<AiFillCloseCircle  size={30} color="black" onClick={showNav}/> }</div> 
-
-
         </div>
+        <div class="menu">
+            {collasped ?
+                <AiOutlineMenu size={30} color="black" onClick={showNav}/> :
+                <AiFillCloseCircle  size={30} color="black" onClick={showNav}/> }
+            </div> 
+        </div >
 
 
     )
