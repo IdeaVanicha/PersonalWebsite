49a984d4de1a9631b2aeb8d33486c84274604589
Idea Vanicha <74161087+IdeaVanicha@users.noreply.github.com>
2022-01-15 08:36:00 +1100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,7 +30,7 @@ export default function Home() {
 
             <div class="text">
                 <h1 id="intro-text">Hi! I'm <span style={{color: "#ED755A"}}>Idea</span></h1>
-                <p id="intro-caption">Aspiring Font-End Developer</p>
+                <p id="intro-caption">Aspiring Front-End Developer</p>
                 <p id="intro-des">Full time data science and finance student at USYD. Part time innovating ideas and pretty design using code as a medium.</p>
                 <div id="contact-button">
                     <a id='button-txt' href="mailto:ideavanicha@gmail.com">Contact Me</a>
